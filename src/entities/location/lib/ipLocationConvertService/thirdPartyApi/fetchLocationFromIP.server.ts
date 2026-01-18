a37a6f2b169998e1a69a3b15fd175bc7c751fd18
DongGeunYoon <ehdrmsdl9999@naver.com>
2026-01-19 04:04:16 +0900

    [refactor] ip기반 위치 찾기 https 가능한 thirdParty api로 변경
    ## 설명
    https 사용가능한 api로 변경

--- src/entities/location/lib/ipLocationConvertService/thirdPartyApi/fetchLocationFromIP.server.ts
+++ src/entities/location/lib/ipLocationConvertService/thirdPartyApi/fetchLocationFromIP.server.ts
@@ -1,20 +1,21 @@
 import { Coordinate } from "@shared/model";
 
 type Response = {
-  lat: number;
-  lon: number;
+  latitude: number;
+  longitude: number;
   countryCode: string;
 };
 
 // ThidParty
 export async function fetchLocationFromIP(ip: string): Promise<{ coordinate: Coordinate; isKorea: boolean }> {
-  const response = await fetch(`http://ip-api.com/json/${ip}`);
+  const response = await fetch(`https://free.freeipapi.com/api/json/${ip}`);
 
   const data: Response = await response.json();
+
   return {
     coordinate: {
-      lat: data.lat,
-      lng: data.lon,
+      lat: data.latitude,
+      lng: data.longitude,
     },
     isKorea: data.countryCode === "KR",
   };
