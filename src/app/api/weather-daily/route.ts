17f88412976ae1864b99d230eb61ff4fec456e0b
DongGeunYoon <ehdrmsdl9999@naver.com>
2026-01-19 18:48:06 +0900

    [feat] production 환경 캐싱 여부 확인 - /weather-current만 캐싱 되는 것 확인하여 다른 것도 확인

--- src/app/api/weather-daily/route.ts
+++ src/app/api/weather-daily/route.ts
@@ -1,10 +1,10 @@
 import { ApiResponse, ParcelAddress } from "@shared/model";
 import { NextRequest, NextResponse } from "next/server";
-import { unstable_cache } from "next/cache";
+import { cacheLife, cacheTag } from "next/cache";
 
 import { geocoder } from "@shared/lib/geocoder";
 import { weatherService } from "@entities/weather/lib";
-import { FetchWeatherDailyResponse, getSecondsUntilNextDailyWeatherUpdate } from "@entities/weather";
+import { FetchWeatherDailyResponse, getSecondsUntilNextTenMinutes } from "@entities/weather";
 import { WeatherDailyApiSearchParams } from "@entities/weather/api/fetchWeatherDaily";
 
 // api route의 경우 반드시 app 폴더에 파일이 위치해야 캐싱됨!! 이동 금지.
@@ -12,7 +12,7 @@ export async function GET(request: NextRequest) {
   const { address } = parseParams(request.nextUrl.searchParams);
   if (!address) return NextResponse.json<ApiResponse>({ error: "잘못된 파라미터", data: null }, { status: 400 });
 
-  const res = await getCachedDailyWeatherByAddress(address);
+  const res = await fetchDailyWeatherByAddress(address);
   return NextResponse.json<FetchWeatherDailyResponse>({ data: res }, { status: 200 });
 }
 
@@ -22,17 +22,24 @@ function parseParams(params: URLSearchParams): WeatherDailyApiSearchParams {
 }
 
 export async function fetchDailyWeatherByAddress(address: ParcelAddress) {
+  "use cache";
   const coordinate = geocoder.geoCode(address);
+  cacheLife({
+    stale: 300,
+    revalidate: getSecondsUntilNextTenMinutes(),
+    expire: getSecondsUntilNextTenMinutes(),
+  });
+  cacheTag("currentWeather", address);
   return await weatherService.fetchDailyWeatherByCoordinates(coordinate);
 }
-const getCachedDailyWeatherByAddress = (address: string) =>
-  unstable_cache(
-    async (address: string) => {
-      return await fetchDailyWeatherByAddress(address);
-    },
-    [`daily-${address}`],
-    {
-      revalidate: getSecondsUntilNextDailyWeatherUpdate(),
-      tags: [`daily-${address}`],
-    }
-  )(address);
+// const getCachedDailyWeatherByAddress = (address: string) =>
+//   unstable_cache(
+//     async (address: string) => {
+//       return await fetchDailyWeatherByAddress(address);
+//     },
+//     [`daily-${address}`],
+//     {
+//       revalidate: getSecondsUntilNextDailyWeatherUpdate(),
+//       tags: [`daily-${address}`],
+//     }
+//   )(address);
